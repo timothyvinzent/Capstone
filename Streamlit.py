5b88b27ceaca03e2eea16dfef13394129f01671f
Timothy Sulzer <timothy.sulzer@gmail.com>
2023-05-07 19:05:57 +0200

    edit

--- Streamlit.py
+++ Streamlit.py
@@ -3,9 +3,12 @@ import streamlit.components.v1 as com
 from streamlit_js_eval import streamlit_js_eval, copy_to_clipboard, create_share_link, get_geolocation
 from PIL import Image
 import base64
+import cdsapi
 
 spring_connect_logo = Image.open("v7_23.png")
 st.image(spring_connect_logo)
+
+
 
 #st.title("Spring Connect")
 
@@ -24,6 +27,12 @@ tab1.subheader("Please fill in the form below to tell us more about you")
 st.write("We will need to know your exact location to determine optimal crop composition and fertilizer type for your farm. Please allow us to access your location.")
 
 location = get_geolocation()
+
+# get a satelit image
+
+# get wather history for the location
+
+# get rain history and weather forecast
 
 st.write("Your location is: ", location)
 
