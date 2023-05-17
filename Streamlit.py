913f6f81c57b2a555cd53d923fa602225803b819
Timothy Sulzer <timothy.sulzer@gmail.com>
2023-05-17 18:33:34 +0200

    edit

--- Streamlit.py
+++ Streamlit.py
@@ -1,38 +1,76 @@
 import streamlit as st
+st.set_page_config(layout="wide")
 import streamlit.components.v1 as com
 from streamlit_js_eval import streamlit_js_eval, copy_to_clipboard, create_share_link, get_geolocation
 from PIL import Image
-
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+from streamlit_capstone_functions import plot_creator_plotly, plot_creator_rcp_plotly
+import os
 
 spring_connect_logo = Image.open("v7_23.png")
 st.image(spring_connect_logo)
-
-
-
-#st.title("Spring Connect")
-
 st.header("Welcome to Spring Connect")
-
 st.subheader("Spring Connect is a platform for connecting farmers to access the seed & fertilizer market and other services")
-
-#st.divider()
-
-
 tab1, tab2 = st.tabs(["🗃 Tell us more", "📈 Welcome"])
-
-
 tab1.subheader("Please fill in the form below to tell us more about you")
 
-st.write("We will need to know your exact location to determine optimal crop composition and fertilizer type for your farm. Please allow us to access your location.")
-
+# get the location of the user
 location = get_geolocation()
+latitude = location["coords"]["latitude"]
+longitude = location["coords"]["longitude"]
+st.write("Your location is: ", latitude, longitude)
 
+# read the data from the csv files
+path = "D:/Git repos/Capstone 2/Capstone/Streamlit small"
+path_lst = os.listdir(path)
+
+@st.cache_data
+def load_data(file):
+    df = pd.read_csv(file)
+    return df
+
+dfs = {}
+for file in path_lst:
+    df_name = os.path.splitext(file)[0]
+    dfs[df_name] = load_data(f"{path}/{file}")
+
+df_names = list(dfs.keys())
+with tab1:
+    col1, col2, col3 = st.columns(3)
+
+    st.write("We will need to know your exact location to determine optimal crop composition and fertilizer type for your farm. Please allow us to access your location.")
+    selected_df_name = st.selectbox("Select a dataframe", df_names)
+    df = dfs[selected_df_name]
+    if selected_df_name in ["rcp45", "rcp85"]:
+        plot_creator_plotly = plot_creator_rcp_plotly
+        column_names = list(df.columns)
+        selected_column_name = st.selectbox("Select a column", column_names[3:])
+        title = st.text_input("Enter a title", f"{selected_column_name} vs time")
+        y_label = st.text_input("Enter a y-axis label", selected_column_name)
+    else:
+        column_names = list(df.columns)
+        #select the last column
+        selected_column_name = column_names[-1]
+        title = f"{selected_column_name} vs time"
+        y_label = selected_column_name
+    fig = plot_creator_plotly(lat=latitude, lon=longitude, df=df, column_name=selected_column_name, title=title, y_label=y_label)
+    #fig.update_layout(height=500, width=10000)
+    st.plotly_chart(figure_or_data = fig, theme= None, use_container_width=True)
+
+
+    #if st.button("Display"):
+    #    for df_name in df_names:
+    #        if df_name != selected_df_name:
+    #            fig = plot_creator_plotly(lat=latitude, lon=longitude, df=dfs[df_name], column_name=selected_column_name, title=title, y_label=y_label)
+    #            st.plotly_chart(fig)
 # get a satelit image
 
 # get wather history for the location
 
 # get rain history and weather forecast
 
-st.write("Your location is: ", location)
+#st.write("Your location is: ", location)
 
 tab2.subheader("What we have found for you")
---
+++ streamlit_capstone_functions.py
@@ -0,0 +1,112 @@
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import plotly.graph_objs as go
+
+
+def check_point_within_range(u_lat, u_lon, lat_range, long_range):
+    if lat_range[0] <= u_lat <= lat_range[1] and long_range[0] <= u_lon <= long_range[1]:
+        return True
+    else:
+        return False
+
+def closest(lst, n):
+    return min(lst, key=lambda x: abs(x-n))
+
+
+
+def plot_creator_plotly(lat, lon, df, column_name, title, y_label):
+    switzerland_lat_range = (45.8172, 47.8087)
+    switzerland_long_range = (5.9569, 10.4922)
+
+    france_lat_range = (41.3712, 51.0918)
+    france_long_range = (-5.1428, 9.5600)
+
+    rwanda_lat_range = (-2.8400, -1.0471)
+    rwanda_long_range = (28.8614, 30.8958)
+
+    u_lat = lat
+    u_lon = lon
+
+    country_loc = [(switzerland_lat_range, switzerland_long_range), (france_lat_range, france_long_range), (rwanda_lat_range, rwanda_long_range)]
+    countries = ['Switzerland', 'France', 'Rwanda']
+
+    checkpoint = False
+    your_country= []
+
+    for i in range(len(country_loc)):
+        if check_point_within_range(u_lat, u_lon, country_loc[i][0], country_loc[i][1]) == True:
+            checkpoint = True
+            your_country.append(countries[i])
+            continue
+        else:
+            continue
+    if checkpoint == False:
+        print("Your location is not within the range of the data we have")
+    else:
+        print(f"Your location is within the range of {your_country[0]}")
+
+    df_filtered = df[(df['lon'].round(1) == round(u_lon, 1)) & (df['lat'].round(1) == round(u_lat, 1))]
+
+    df_filtered['time'] = pd.to_datetime(df_filtered['time'], format='%Y-%m-%d %H:%M:%S')
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=df_filtered['time'], y=df_filtered[column_name], mode='lines', name='Temperature'))
+
+    fig.update_layout(title=title,
+                      xaxis_title='Time',
+                      yaxis_title=y_label)
+
+    return fig
+
+import pandas as pd
+import plotly.graph_objs as go
+
+def plot_creator_rcp_plotly(lat, lon, df, column_name, title, y_label):
+    switzerland_lat_range = (45.8172, 47.8087)
+    switzerland_long_range = (5.9569, 10.4922)
+
+    france_lat_range = (41.3712, 51.0918)
+    france_long_range = (-5.1428, 9.5600)
+
+    rwanda_lat_range = (-2.8400, -1.0471)
+    rwanda_long_range = (28.8614, 30.8958)
+
+    u_lat = lat
+    u_lon = lon
+
+    country_loc = [(switzerland_lat_range, switzerland_long_range), (france_lat_range, france_long_range), (rwanda_lat_range, rwanda_long_range)]
+    countries = ['Switzerland', 'France', 'Rwanda']
+
+    checkpoint = False
+    your_country= []
+
+    for i in range(len(country_loc)):
+        if check_point_within_range(u_lat, u_lon, country_loc[i][0], country_loc[i][1]) == True:
+            checkpoint = True
+            your_country.append(countries[i])
+            continue
+        else:
+            continue
+    if checkpoint == False:
+        print("Your location is not within the range of the data we have")
+    else:
+        print(f"Your location is within the range of {your_country[0]}")
+
+    df_filtered = df[(df["lon"] == closest(df["lon"].unique().tolist(), u_lon)) & (df["lat"] == closest(df["lat"].unique().tolist(), u_lat))]
+
+    df_filtered['time'] = pd.to_datetime(df_filtered['time'], format='%Y-%m-%d %H:%M:%S')
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=df_filtered['time'], y=df_filtered[column_name], name=column_name))
+
+    fig.update_layout(title=title,
+                      xaxis_title='Time',
+                      yaxis_title=y_label)
+
+    return fig
+
