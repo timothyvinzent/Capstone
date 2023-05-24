69a0179009ab34ecb849bf23e564a88b433a5fa1
Timothy Sulzer <timothy.sulzer@gmail.com>
2023-05-24 22:54:16 +0200

    df

--- Streamlit.py
+++ Streamlit.py
@@ -46,8 +46,20 @@ with tab1:
         if st.button("Location 3"):
             st.write("You selected location 3")
             var_location = 3
+    st.set_page_config(layout='wide')
 
-    st.write("Your location is: ", latitude, longitude)
+    if var_location in [1,2,3]:
+        st.write(f"Thank you for selecting a location, we are now loading the data. This is only a demo, in the real thing we would access our entire 200gb database using your current location which is {latitude}, {longitude}")
+        if var_location == 1:
+            path = "Image1"
+        elif var_location == 2:
+            path = "Image2"
+        elif var_location == 3:
+            path = "Image3"
+
+
+
+    #st.write("Your location is: ", latitude, longitude)
     fake_lat = 47.43478468940344 
     fake_lon = 9.383939772613207
 # read the data from the csv files
@@ -73,47 +85,48 @@ with tab1:
 #     df_name = os.path.splitext(file)[0]
 #     dfs[df_name] = load_data(f"{path}/{file}")
 
-# Get the absolute path of your script
-script_path = os.path.abspath(__file__)
+    # Get the absolute path of your script
+    script_path = os.path.abspath(__file__)
 
-# Construct the path to your data files using the script path
-data_path = os.path.join(os.path.dirname(script_path), "Streamlit small")
+    # Construct the path to your data files using the script path
+    data_path = os.path.join(os.path.dirname(script_path), path)
 
-# Get a list of all CSV files in the data folder
-csv_files = [f for f in os.listdir(data_path) if f.endswith(".csv")]
+    # Get a list of all CSV files in the data folder
+    csv_files = [f for f in os.listdir(data_path) if f.endswith(".csv")]
 
-@st.cache_data
-def load_data(file):
-    df = pd.read_csv(file)
-    return df
+    @st.cache_data
+    def load_data(file):
+        df = pd.read_csv(file)
+        return df
 
-dfs = {}
-for file in csv_files:
-    df_name = os.path.splitext(file)[0]
-    dfs[df_name] = load_data(os.path.join(data_path, file))
+    dfs = {}
+    for file in csv_files:
+        df_name = os.path.splitext(file)[0]
+        dfs[df_name] = load_data(os.path.join(data_path, file))
+    st.write(dfs)
 
-df_names = list(dfs.keys())
-with tab1:
-    col1, col2, col3 = st.columns(3)
+# df_names = list(dfs.keys())
+# with tab1:
+#     col1, col2, col3 = st.columns(3)
 
-    st.write("We will need to know your exact location to determine optimal crop composition and fertilizer type for your farm. Please allow us to access your location.")
-    selected_df_name = st.selectbox("Select a dataframe", df_names)
-    df = dfs[selected_df_name]
-    if selected_df_name in ["rcp45", "rcp85"]:
-        plot_creator_plotly = plot_creator_rcp_plotly
-        column_names = list(df.columns)
-        selected_column_name = st.selectbox("Select a column", column_names[3:])
-        title = st.text_input("Enter a title", f"{selected_column_name} vs time")
-        y_label = st.text_input("Enter a y-axis label", selected_column_name)
-    else:
-        column_names = list(df.columns)
-        #select the last column
-        selected_column_name = column_names[-1]
-        title = f"{selected_column_name} vs time"
-        y_label = selected_column_name
-    fig = plot_creator_plotly(lat=fake_lat, lon=fake_lon, df=df, column_name=selected_column_name, title=title, y_label=y_label)
-    #fig.update_layout(height=500, width=10000)
-    st.plotly_chart(figure_or_data = fig, theme= None, use_container_width=True)
+#     st.write("We will need to know your exact location to determine optimal crop composition and fertilizer type for your farm. Please allow us to access your location.")
+#     selected_df_name = st.selectbox("Select a dataframe", df_names)
+#     df = dfs[selected_df_name]
+#     if selected_df_name in ["rcp45", "rcp85"]:
+#         plot_creator_plotly = plot_creator_rcp_plotly
+#         column_names = list(df.columns)
+#         selected_column_name = st.selectbox("Select a column", column_names[3:])
+#         title = st.text_input("Enter a title", f"{selected_column_name} vs time")
+#         y_label = st.text_input("Enter a y-axis label", selected_column_name)
+#     else:
+#         column_names = list(df.columns)
+#         #select the last column
+#         selected_column_name = column_names[-1]
+#         title = f"{selected_column_name} vs time"
+#         y_label = selected_column_name
+#     fig = plot_creator_plotly(lat=fake_lat, lon=fake_lon, df=df, column_name=selected_column_name, title=title, y_label=y_label)
+#     #fig.update_layout(height=500, width=10000)
+#     st.plotly_chart(figure_or_data = fig, theme= None, use_container_width=True)
 
 
     #if st.button("Display"):
